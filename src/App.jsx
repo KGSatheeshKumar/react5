fa8e14e911b254cf1e48fbaab55848b1768f723b
KGSatheeshKumar <kgsatheeshkumar66@gmail.com>
2024-07-26 20:22:49 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 
 const mockCharacters = [
-  { id: 1, name: 'Rick Sanchez', age: '60', image: './src/img/vijay.jpg' },
-  { id: 2, name: 'Morty Smith', age: '10', image: './src/img/ajith.jpg' },
-  { id: 3, name: 'Summer Smith', age: '26', image: './src/img/surya.jpg' },
-  { id: 4, name: 'Beth Smith', age: '32', image: './src/img/rajini.jpg' },
-  { id: 5, name: 'Jerry Smith', age: '35', image: './src/img/kamal.jpg' },
-  { id: 6, name: 'Birdperson', age: '30', image: './src/img/jack sparrow.jpg' },
-  { id: 7, name: 'Squanchy', age: '45', image: './src/img/rdj.jpg' },
-  { id: 8, name: 'Mr. Poopybutthole', age: '50', image: './src/img/thor.jpg' },
-  { id: 9, name: 'Scary Terry', age: '40', image: './src/img/tom.jpg' },
-  { id: 10, name: 'Unity', age: '37', image: './src/img/wolvarine.jpg' }
+  { id: 1, name: 'Rick Sanchez', age: '60', image: './src/vijay.jpg' },
+  { id: 2, name: 'Morty Smith', age: '10', image: './src/ajith.jpg' },
+  { id: 3, name: 'Summer Smith', age: '26', image: './src/surya.jpg' },
+  { id: 4, name: 'Beth Smith', age: '32', image: './src/rajini.jpg' },
+  { id: 5, name: 'Jerry Smith', age: '35', image: './src/kamal.jpg' },
+  { id: 6, name: 'Birdperson', age: '30', image: './src/jack sparrow.jpg' },
+  { id: 7, name: 'Squanchy', age: '45', image: './src/rdj.jpg' },
+  { id: 8, name: 'Mr. Poopybutthole', age: '50', image: './src/thor.jpg' },
+  { id: 9, name: 'Scary Terry', age: '40', image: './src/tom.jpg' },
+  { id: 10, name: 'Unity', age: '37', image: './src/wolvarine.jpg' }
 ];
 
 const App = () => {
